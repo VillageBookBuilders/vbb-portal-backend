b4e54b5b54dce37bb34dc5dad11b58f3c00f029f
Vignesh Hari <vichuhari100@gmail.com>
2022-04-22 02:50:46 +0530

    Added program reference in computer

--- vbb/meetings/api/serializers/computer.py
+++ vbb/meetings/api/serializers/computer.py
@@ -1,9 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from vbb.meetings.api.serializers.program import ProgramSerializer
 
-from vbb.meetings.models import Computer
+from vbb.meetings.models import Computer, Program
 
 
 class ComputerSerializer(ModelSerializer):
+
+    program = PrimaryKeyRelatedField(queryset=Program.objects.all(), required=True)
+    program_object = ProgramSerializer(source="program", read_only=True)
+
     class Meta:
         model = Computer
         fields = "__all__"
+        read_only_fields = ["deleted"]
