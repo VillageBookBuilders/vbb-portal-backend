732678693d43d45601d9f8a4f51ca69d96889887
Vignesh Hari <vichuhari100@gmail.com>
2022-05-27 23:16:31 +0530

    Added slot endpoints

--- vbb/meetings/api/serializers/slot.py
+++ vbb/meetings/api/serializers/slot.py
@@ -1,7 +1,10 @@
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from datetime import timedelta
+from django.forms import ValidationError
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, IntegerField
 from vbb.meetings.api.serializers.computer import ComputerSerializer
 
-from vbb.meetings.models import Slot,Computer
+from vbb.meetings.models import Slot, Computer
+from rest_framework.exceptions import ValidationError
 
 
 class SlotSerializer(ModelSerializer):
@@ -9,6 +12,28 @@ class SlotSerializer(ModelSerializer):
     computer = PrimaryKeyRelatedField(queryset=Computer.objects.all(), required=True)
     computer_object = ComputerSerializer(source="computer", read_only=True)
 
+    start_day_of_the_week = IntegerField(required=True)
+    end_day_of_the_week = IntegerField(required=True)
+    start_hour = IntegerField(required=True)
+    end_hour = IntegerField(required=True)
+    start_minute = IntegerField(required=True)
+    end_minute = IntegerField(required=True)
+
+    def validate(self, attrs):
+        self.start_time = Slot.DEAFULT_INIT_DATE + timedelta(
+            days=attrs.pop("start_day_of_the_week"), hours=attrs.pop("start_hour"), minutes=attrs.pop("start_minute")
+        )
+        self.end_time = Slot.DEAFULT_INIT_DATE + timedelta(
+            days=attrs.pop("end_day_of_the_week"), hours=attrs.pop("end_hour"), minutes=attrs.pop("end_minute")
+        )
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        validated_data["schedule_start"] = self.start_time
+        validated_data["schedule_end"] = self.end_time
+
+        return super().create(validated_data)
+
     class Meta:
         model = Slot
-        fields = "__all__"
+        exclude = ["schedule_start", "schedule_end"]
