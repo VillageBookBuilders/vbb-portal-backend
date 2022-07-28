6a6cfe5377f1eab3177fd68014d03205ba561c15
Christopher Richards <ccrichards1996@gmail.com>
2022-07-28 03:01:15 -0500

    CORS update

--- config/settings/base.py
+++ config/settings/base.py
@@ -456,6 +456,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://jellyfish-app-7iuqd.ondigitalocean.app"
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
