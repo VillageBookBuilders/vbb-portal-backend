28ec345ed84204c3f328f1d9ad68d5b822651a24
Aleem <aleemkirk@gmail.com>
2022-08-09 13:28:33 -0400

    adds library comp slot list filter

--- vbb/libraries/admin.py
+++ vbb/libraries/admin.py
@@ -12,25 +12,9 @@ admin.site.register(Announcement)
 @admin.register(LibraryComputerSlots)
 class LibraryComputerSlotsAdmin(admin.ModelAdmin):
 
-    # fieldsets = (
-    #     (None, {"fields": ["password"]}),
-    #     (_("Personal info"), {"fields": ("name", "email", "first_name","last_name", "profileImage", "role","is_student", "is_mentor", "is_librarian")}),
-    #     (
-    #         _("Permissions"),
-    #         {
-    #             "fields": (
-    #                 "is_active",
-    #                 "is_staff",
-    #                 "is_superuser",
-    #                 "groups",
-    #                 "user_permissions",
-    #             ),
-    #         },
-    #     ),
-    #     (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    # )
-    list_display = ["get_library", "day", "start_time", "end_time", ]
+    list_display = ["get_library", "day", "start_time", "end_time", "start_recurring" ,"end_recurring"]
     search_fields = ["library__name"]
+    list_filter = ("day")
 
     @admin.display(ordering='library__name', description='Library')
     def get_library(self, obj):
