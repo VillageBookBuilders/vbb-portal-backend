337f61aa73ca29b3aab1e89fbf64ca2620748252
Aleem <aleemkirk@gmail.com>
2022-08-09 16:28:52 -0400

    model admin fix

--- vbb/libraries/admin.py
+++ vbb/libraries/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Library, Computer, LibraryComputerSlots, UserPreferenceSlot, ComputerReservation, Announcement
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 admin.site.register(Library)
