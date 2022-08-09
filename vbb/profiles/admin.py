8328af36fd58959417695aa1be920d100ab7d80e
Aleem <aleemkirk@gmail.com>
2022-08-09 10:36:35 -0400

    mentor profile admin view

--- vbb/profiles/admin.py
+++ vbb/profiles/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 from .models import AdvisorProfile, MentorProfile, LibrarianProfile, StudentProfile, Opportunity
 
 # Register your models here.
@@ -7,3 +8,26 @@ admin.site.register(AdvisorProfile)
 admin.site.register(LibrarianProfile)
 admin.site.register(StudentProfile)
 admin.site.register(Opportunity)
+
+@admin.register(MentorProfile)
+class MentorProfileAdmin(auth_admin.MentorProfileAdmin):
+
+    # fieldsets = (
+    #     (None, {"fields": ["password"]}),
+    #     (_("Personal info"), {"fields": ("name", "email", "first_name","last_name", "profileImage", "role","is_student", "is_mentor", "is_librarian")}),
+    #     (
+    #         _("Permissions"),
+    #         {
+    #             "fields": (
+    #                 "is_active",
+    #                 "is_staff",
+    #                 "is_superuser",
+    #                 "groups",
+    #                 "user_permissions",
+    #             ),
+    #         },
+    #     ),
+    #     (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    # )
+    list_display = ["user"]
+    search_fields = []
