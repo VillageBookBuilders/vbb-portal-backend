80c63383774b943c14dfcf40f247f44c9c8753b5
Aleem <aleemkirk@gmail.com>
2022-08-09 13:16:03 -0400

    LibraryComputerSlot admin view change

--- vbb/libraries/admin.py
+++ vbb/libraries/admin.py
@@ -4,7 +4,34 @@ from .models import Library, Computer, LibraryComputerSlots, UserPreferenceSlot,
 # Register your models here.
 admin.site.register(Library)
 admin.site.register(Computer)
-admin.site.register(LibraryComputerSlots)
 admin.site.register(UserPreferenceSlot)
 admin.site.register(ComputerReservation)
 admin.site.register(Announcement)
+
+
+@admin.register(LibraryComputerSlots)
+class LibraryComputerSlotsAdmin(admin.ModelAdmin):
+
+    # fieldsets = (
+    #     (None, {"fields": ["password"]}),
+    #     (_("Personal info"), {"fields": ("name", "email", "first_name","last_name", "profileImage", "role","is_student", "is_mentor", "is_librarian")}),
+    #     (
+    #         _("Permissions"),
+    #         {
+    #             "fields": (
+    #                 "is_active",
+    #                 "is_staff",
+    #                 "is_superuser",
+    #                 "groups",
+    #                 "user_permissions",
+    #             ),
+    #         },
+    #     ),
+    #     (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    # )
+    list_display = ["get_library", "day", "start_time", "end_time", ]
+    search_fields = ["library__name"]
+
+    @admin.display(ordering='library__name', description='Library')
+    def get_library(self, obj):
+        return obj.library.name
