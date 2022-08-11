f0bd33f33458a79092b17d6deb4377bada0541dd
Aleem <aleemkirk@gmail.com>
2022-08-11 12:33:43 -0400

    advisor fix

--- vbb/profiles/admin.py
+++ vbb/profiles/admin.py
@@ -9,7 +9,7 @@ class MentorProfileAdmin(admin.ModelAdmin):
 
     list_display = ["get_name", "assigned_library", "get_verified", "approval_status"]
     search_fields = ["user__first_name", "user__last_name", "user__username"]
-    list_filter = ("approval_status", "organization__name", "assigned_library__name", "user__is_email_verified")
+    list_filter = ("approval_status", "organization", "assigned_library", "user__is_email_verified")
 
     @admin.display(ordering='user__name', description='name')
     def get_name(self, obj):
@@ -32,7 +32,7 @@ class StudentProfileAdmin(admin.ModelAdmin):
 
     list_display = ["get_name", "assigned_library", "approval_status"]
     search_fields = ["user__first_name", "user__last_name", "user__username"]
-    list_filter = ("approval_status", "assigned_library__name")
+    list_filter = ("approval_status", "assigned_library")
 
     @admin.display(ordering='user__name', description='name')
     def get_name(self, obj):
@@ -44,9 +44,9 @@ class StudentProfileAdmin(admin.ModelAdmin):
 @admin.register(AdvisorProfile)
 class AdvisorProfileAdmin(admin.ModelAdmin):
 
-    list_display = ["library__name", "get_name"]
+    list_display = ["library", "get_name"]
     search_fields = ["user__first_name", "user__last_name", "user__username"]
-    list_filter = ("library__name")
+    list_filter = ("library")
 
     @admin.display(ordering='user__name', description='name')
     def get_name(self, obj):
@@ -60,9 +60,10 @@ class AdvisorProfileAdmin(admin.ModelAdmin):
 @admin.register(LibrarianProfile)
 class LibrarianProfileAdmin(admin.ModelAdmin):
 
-    list_display = ["library__name", "get_name"]
+    list_display = ["library", "get_name"]
     search_fields = ["user__first_name", "user__last_name", "user__username"]
-    list_filter = ("library__name")
+    list_filter = ("library")
+
 
     @admin.display(ordering='user__name', description='name')
     def get_name(self, obj):
