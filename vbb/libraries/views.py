b5f40787e54a0af6b8cab2f20e6fd674b9387a0f
Christopher Richards <ccrichards1996@gmail.com>
2022-08-11 15:29:28 -0500

    datetime update 3

--- vbb/libraries/views.py
+++ vbb/libraries/views.py
@@ -921,11 +921,13 @@ class UserPreferenceSlotViews(APIView):
 
 
                             if isinstance(userSlot.start_time, str):
-                                start = datetime.strptime(userSlot.start_time, '%Y-%m-%dT%H:%M:%S%fZ')
-                                end = datetime.strptime(userSlot.end_time, '%Y-%m-%dT%H:%M:%S%fZ')
+                                start = datetime.strptime(userSlot.start_time, '%Y-%m-%dT%H:%M:%S%fZ').strftime('%Y-%m-%dT%H:%M:%S%fZ')
+                                end = datetime.strptime(userSlot.end_time, '%Y-%m-%dT%H:%M:%S%fZ').strftime('%Y-%m-%dT%H:%M:%S%fZ')
                             else:
                                 start = userSlot.start_time.strftime('%Y-%m-%dT%H:%M:%S%fZ')
                                 end = userSlot.end_time.strftime('%Y-%m-%dT%H:%M:%S%fZ')
+                            print(start)
+                            print(end)
 
 
                             conferenceLink = generateCalendarEvent(username, mentor.email, directorEmail, start, end, mentor.email)
