7db731a4b78606a9abf24352e84fdc940a9465dc
Aleem <aleemkirk@gmail.com>
2022-08-10 20:33:06 -0400

    error fix

--- vbb/libraries/admin.py
+++ vbb/libraries/admin.py
@@ -60,11 +60,17 @@ class ComputerReservationAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='student__name', description='student')
     def get_student(self, obj):
-        return (obj.student.first_name + " " +obj.student.last_name)
+        try:
+            return (obj.student.first_name + " " +obj.student.last_name)
+        except:
+            return "----"
     
     @admin.display(ordering='mentor__name', description='mentor')
     def get_mentor(self, obj):
-        return (obj.mentor.first_name + " " +obj.mentor.last_name)
+        try:
+            return (obj.mentor.first_name + " " +obj.mentor.last_name)
+        except:
+            return "----"
 
     @admin.display(ordering='library__name', description='library')
     def get_library(self, obj):
