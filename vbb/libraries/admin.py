02211be661e2b1c5b75c587edc5e3d34bad19f1b
Aleem <aleemkirk@gmail.com>
2022-08-10 20:00:35 -0400

    NoneType error fix

--- vbb/libraries/admin.py
+++ vbb/libraries/admin.py
@@ -24,21 +24,30 @@ class LibraryComputerSlotsAdmin(admin.ModelAdmin):
 class UserPreferenceSlotAdmin(admin.ModelAdmin):
 
 
-    # list_display = ["get_library", "get_student", "get_mentor", "is_recurring"]
-    # search_fields = ["student__first_name", "student__last_name"]
-    # list_filter = ("mentor", "student")
+    list_display = ["get_library", "get_student", "get_mentor", "is_recurring"]
+    search_fields = ["student__first_name", "student__last_name"]
+    list_filter = ("mentor", "student")
 
     @admin.display(ordering='student__name', description='student')
     def get_student(self, obj):
-        return (obj.student.first_name + " " +obj.student.last_name)
+        try:
+            return (obj.student.first_name + " " +obj.student.last_name)
+        except:
+            return ""
     
     @admin.display(ordering='mentor__name', description='mentor')
     def get_mentor(self, obj):
-        return (obj.mentor.first_name + " " +obj.mentor.last_name)
+        try:
+            return (obj.mentor.first_name + " " +obj.mentor.last_name)
+        except:
+            return ""
 
     @admin.display(ordering='library__name', description='library')
     def get_library(self, obj):
-        return obj.computer_slot.library.name
+        try:
+            return obj.computer_slot.library.name
+        except:
+            return ""
 
 
 @admin.register(ComputerReservation)
