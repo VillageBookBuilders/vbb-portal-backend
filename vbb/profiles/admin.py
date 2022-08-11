ab945b0f1ab81730b0385ff077d706e4ee5eecd1
Aleem <aleemkirk@gmail.com>
2022-08-11 12:16:34 -0400

    admin profile registrations

--- vbb/profiles/admin.py
+++ vbb/profiles/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import AdvisorProfile, MentorProfile, LibrarianProfile, StudentProfile, Opportunity
 
 # Register your models here.
-admin.site.register(AdvisorProfile)
-admin.site.register(LibrarianProfile)
-admin.site.register(StudentProfile)
 admin.site.register(Opportunity)
 
 @admin.register(MentorProfile)
@@ -12,9 +9,9 @@ class MentorProfileAdmin(admin.ModelAdmin):
 
     list_display = ["get_name", "assigned_library", "get_verified", "approval_status"]
     search_fields = ["user__first_name", "user__last_name", "user__username"]
-    list_filter = ("approval_status", "organization", "assigned_library", "user__is_email_verified")
+    list_filter = ("approval_status", "organization__name", "assigned_library__name", "user__is_email_verified")
 
-    @admin.display(ordering='user__first_name', description='name')
+    @admin.display(ordering='user__name', description='name')
     def get_name(self, obj):
         try: 
             return (obj.user.first_name + " " +obj.user.last_name)
@@ -25,5 +22,51 @@ class MentorProfileAdmin(admin.ModelAdmin):
     def get_verified(self, obj):
         try: 
             return obj.user.is_email_verified
+        except:
+            return "----"
+
+
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+
+    list_display = ["get_name", "assigned_library", "approval_status"]
+    search_fields = ["user__first_name", "user__last_name", "user__username"]
+    list_filter = ("approval_status", "assigned_library__name")
+
+    @admin.display(ordering='user__name', description='name')
+    def get_name(self, obj):
+        try: 
+            return (obj.user.first_name + " " +obj.user.last_name)
+        except:
+            return "----"
+
+@admin.register(AdvisorProfile)
+class AdvisorProfileAdmin(admin.ModelAdmin):
+
+    list_display = ["library__name", "get_name"]
+    search_fields = ["user__first_name", "user__last_name", "user__username"]
+    list_filter = ("library__name")
+
+    @admin.display(ordering='user__name', description='name')
+    def get_name(self, obj):
+        try: 
+            return (obj.user.first_name + " " +obj.user.last_name)
+        except:
+            return "----"
+
+
+
+@admin.register(LibrarianProfile)
+class LibrarianProfileAdmin(admin.ModelAdmin):
+
+    list_display = ["library__name", "get_name"]
+    search_fields = ["user__first_name", "user__last_name", "user__username"]
+    list_filter = ("library__name")
+
+    @admin.display(ordering='user__name', description='name')
+    def get_name(self, obj):
+        try: 
+            return (obj.user.first_name + " " +obj.user.last_name)
         except:
             return "----"
