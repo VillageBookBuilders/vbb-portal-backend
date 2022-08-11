0504c072c7801f9bf66b81c53b65e1ecd95318d1
Aleem <aleemkirk@gmail.com>
2022-08-11 11:17:31 -0400

    changed mentor profile model admin

--- vbb/profiles/admin.py
+++ vbb/profiles/admin.py
@@ -10,11 +10,20 @@ admin.site.register(Opportunity)
 @admin.register(MentorProfile)
 class MentorProfileAdmin(admin.ModelAdmin):
 
-    list_display = ["get_first_name", "approval_status"]
-    search_fields = ["user__first_name"]
-    list_filter = ("approval_status", "organization", "assigned_library")
+    list_display = ["get_name", "assigned_library", "get_verified", "approval_status"]
+    search_fields = ["user__first_name", "user__last_name", "user__username"]
+    list_filter = ("approval_status", "organization", "assigned_library", "user__is_email_verified")
 
-    @admin.display(ordering='user__first_name', description='user first name')
-    def get_first_name(self, obj):
-        return obj.user.first_name
+    @admin.display(ordering='user__first_name', description='name')
+    def get_name(self, obj):
+        try: 
+            return (obj.user.first_name + " " +obj.user.last_name)
+        except:
+            return "----"
 
+    @admin.display(ordering='user__is_email_verified', description='Email Verified')
+    def get_verified(self, obj):
+        try: 
+            return obj.user.is_email_verified
+        except:
+            return "----"
