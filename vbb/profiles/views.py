dd84ffebca709fc1b6eba38bb7b04d177e8d1e58
Aleem <aleemkirk@gmail.com>
2022-08-24 13:19:28 -0400

    email string formatting

--- vbb/profiles/views.py
+++ vbb/profiles/views.py
@@ -161,33 +161,22 @@ class MentorSignUp(APIView):
 
 
             body = f"""
-                    Hello {user.first_name},\n
+                    Hello {user.first_name},
 
-                        Thank you for your interest in becoming a mentor and bringing hope to kids through education!! 
-                        Our mentors meet with students around the world for 1 hour each week. They read storybooks, 
-                        learn together online, and develop a relationship with their student over the course of many months. 
-                        Note: Mentors must have a computer they can use for mentor sessions 1 hour each week, and have a strong, 
-                        stable internet connection.\n
+                        Thank you for your interest in becoming a mentor and bringing hope to kids through education!! Our mentors meet with students around the world for 1 hour each week. They read storybooks, learn together online, and develop a relationship with their student over the course of many months. Note: Mentors must have a computer they can use for mentor sessions 1 hour each week, and have a strong, stable internet connection.
 
-                        It takes just a few simple steps to become a mentor:\n
+                        It takes just a few simple steps to become a mentor:
 
-                        1. Click the following link to verify your email address: {link}\n
+                        1. Click the following link to verify your email address: {link}
+                        2. Record a shot 1-2 minute video introducing yourself to your potential child and  explaining why you want to be their mentor. Upload this video to youtube or any other video hosting platform and save the video link for the next step!
+                        3. Log-in and complete your short onboarding questionnaire, then submit for our team to review
+                        4. Book your mentor orientation session via calendly: https://calendly.com/admin-5496/mentor-orientation-and-scheduling
+                        5. Join Facebook Workplace: https://work.me/g/qt70QTGMs/R3dUS5iu
+                        6. Attend your oreintation so that we can help you book your first session!
 
-                        2. Record a shot 1-2 minute video introducing yourself to your potential child and 
-                        explaining why you want to be their mentor. Upload this video to youtube or any other 
-                        video hosting platform and save the video link for the next step!\n
+                        If you have any questions please let us know, we are here to help you make a difference.
 
-                        2. Log-in and complete your short onboarding questionnaire, then submit for our team to review\n
-
-                        3. Book your mentor orientation session via calendly: https://calendly.com/admin-5496/mentor-orientation-and-scheduling\n
-
-                        4. Join Facebook Workplace: https://work.me/g/qt70QTGMs/R3dUS5iu\n
-
-                        5. Attend your oreintation so that we can help you book your first session!\n
-
-                        If you have any questions please let us know, we are here to help you make a difference.\n
-
-                        Thank you,\n
+                        Thank you,
                         Everyone at Village Book Builders"""
             send_mail(
                 "Village Book Builders - Please confirm your email",
