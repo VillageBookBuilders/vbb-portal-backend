85c28221fbf64095ed11b36778675300d69bcb61
Aleem <aleemkirk@gmail.com>
2022-12-09 10:40:24 -0400

    updaes reservation model

--- vbb/libraries/models.py
+++ vbb/libraries/models.py
@@ -109,7 +109,7 @@ class ComputerReservation(models.Model):
     reserved_date = models.DateTimeField(auto_now_add=True)
     reserve_status = models.IntegerField(choices=STATUS, null=True, default=0)
     mentor = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,  blank=True, on_delete=models.SET_NULL, related_name='reservation_mentor')
-    student = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL, related_name='reservation_student')
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,  blank=True, on_delete=models.SET_NULL, related_name='reservation_student')
     computer = models.ForeignKey(Computer, on_delete=models.SET_NULL, null=True)
     transaction_id = models.CharField(max_length=255, unique=True, null=False, blank=False)
     start_time = models.DateTimeField(default=None, blank=True, null=True)
